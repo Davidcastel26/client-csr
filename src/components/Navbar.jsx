4e95cec928fcbd91aa75e4f3693f70e25994135e
David Castellanos <49693134+Davidcastel26@users.noreply.github.com>
2023-11-17 12:51:59 -0600

    Update Navbar.jsx

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -5,8 +5,29 @@ import { FaBarsStaggered } from 'react-icons/fa6'
 export const Navbar = () => {
   return (<nav className="bg-base-200">
     <div className="navbar align-element">
-        <div className="navbar-start"></div>
-        <div className="navbar-center"></div>
+        <div className="navbar-start">
+            {/* TITLE */}
+            <NavLink to='/' className='hidden lg:flex btn btn-primary text-3xl items-center'>
+             CSR
+            </NavLink> 
+            {/* DROPDOWN */}
+            <div className="dropdown">
+                <label 
+                    tabIndex={0}
+                    className="btn btn-ghost lg:hidden"
+                >
+                    <FaBarsStaggered className="h-6 w-6" />
+                </label>
+                <ul tabIndex={0} className="menu menu-sm dropdown-content mt-3 z-[1] p-2 shadow bg-base-200">
+                    nav links
+                </ul>
+            </div>
+        </div>
+        <div className="navbar-center hidden lg:flex">
+            <ul className="menu menu-horizontal">
+                nav links
+            </ul>
+        </div>
         <div className="navbar-end"></div>
     </div>
   </nav>)
