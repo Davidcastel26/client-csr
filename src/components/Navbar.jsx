9827d63229f5f75824580c5ead7167d8259a226d
David Castellanos <49693134+Davidcastel26@users.noreply.github.com>
2023-11-17 12:44:38 -0600

    setting nav

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,13 @@
+import { NavLink } from "react-router-dom"
+import { BsCart3, BsMoonFill, BsSunFill } from 'react-icons/bs'
+import { FaBarsStaggered } from 'react-icons/fa6'
+
+export const Navbar = () => {
+  return (<nav className="bg-base-200">
+    <div className="navbar align-element">
+        <div className="navbar-start"></div>
+        <div className="navbar-center"></div>
+        <div className="navbar-end"></div>
+    </div>
+  </nav>)
+}
