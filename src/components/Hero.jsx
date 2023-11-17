230c17e2a9b41e1c91cec4d4cb66de3856d2e659
David Castellanos <49693134+Davidcastel26@users.noreply.github.com>
2023-11-17 17:54:45 -0600

    Update Hero.jsx

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,16 +1,31 @@
 import { Link } from 'react-router-dom'
 
 import hero1 from '../../public/landingIMGS/outside.jpeg';
-import hero2 from '../../public/landingIMGS/comedor.jpeg';
-import hero3 from '../../public/landingIMGS/cocina.jpeg';
+import hero3 from '../../public/landingIMGS/comedor.jpeg';
+import hero2 from '../../public/landingIMGS/cocina.jpeg';
 import hero4 from '../../public/landingIMGS/pasillo.jpeg';
+
+const carouselImages = [ hero1, hero2, hero3, hero4 ];
 
 export const Hero = () => {
   return (
     <div
-      className='grid lg:grid-cols-2'
+      className='grid lg:grid-cols-2 gap-24 items-center'
     >
-            
+      <div className=""> Info </div>
+      <div 
+        className="hidden h-[28rem] lg:carousel carousel-center p-4 space-x-4 bg-neutral rounded-box"
+      > 
+        {
+          carouselImages.map( (img, index) => {
+            return (
+              <div className="carousel-item" key={img}>
+                <img src={img} className='rounded-box h-full w-80 object-cover' alt='index'/>
+              </div>
+            )
+          })
+        }
+      </div>
     </div>
   )
 }
