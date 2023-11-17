c3e7f6eb6d730c791a93c040c28a75e08c2194f1
David Castellanos <49693134+Davidcastel26@users.noreply.github.com>
2023-11-17 13:29:07 -0600

    navlinks working into mobile nav and desk nav

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -4,6 +4,7 @@ import { FaBed } from "react-icons/fa6";
 // import { BiSolidBed } from "react-icons/bi";
 // import { MdLocalHotel } from "react-icons/md";
 import { FaBarsStaggered } from 'react-icons/fa6'
+import { NavLinks } from "./NavLinks";
 
 export const Navbar = () => {
   return (<nav className="bg-base-200">
@@ -21,14 +22,14 @@ export const Navbar = () => {
                 >
                     <FaBarsStaggered className="h-6 w-6" />
                 </label>
-                <ul tabIndex={0} className="menu menu-sm dropdown-content mt-3 z-[1] p-2 shadow bg-base-200">
-                    nav links
+                <ul tabIndex={0} className="menu menu-sm dropdown-content mt-3 z-[1] p-2 shadow bg-base-200 rounded-box w-80">
+                    <NavLinks />
                 </ul>
             </div>
         </div>
         <div className="navbar-center hidden lg:flex">
             <ul className="menu menu-horizontal">
-                nav links
+                <NavLinks />
             </ul>
         </div>
         <div className="navbar-end">
--- src/utils/links.js
+++ src/utils/links.js
@@ -1,8 +1,8 @@
 export const links = [
     { id: 1, url: '/', text: 'home'},
     { id: 2, url: 'products', text: 'campers'},
+    { id: 6, url: 'about', text: 'about'},
     { id: 3, url: 'bookings', text: 'booking'},
     { id: 4, url: 'checkout', text: 'checkout'},
     { id: 5, url: 'orders', text: 'reservation'},
-    { id: 6, url: 'about', text: 'about'},
 ]
