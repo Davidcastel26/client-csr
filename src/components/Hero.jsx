f85a7264b7a8082019310557f35b9231b704a508
David Castellanos <49693134+Davidcastel26@users.noreply.github.com>
2023-11-17 17:43:42 -0600

    Update Hero.jsx

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,6 +1,16 @@
+import { Link } from 'react-router-dom'
+
+import hero1 from '../../public/landingIMGS/outside.jpeg';
+import hero2 from '../../public/landingIMGS/comedor.jpeg';
+import hero3 from '../../public/landingIMGS/cocina.jpeg';
+import hero4 from '../../public/landingIMGS/pasillo.jpeg';
 
 export const Hero = () => {
   return (
-    <div>Hero</div>
+    <div
+      className='grid lg:grid-cols-2'
+    >
+            
+    </div>
   )
 }
